06d09d78a745527de785bbcb536f83be40127bbe
jnvillalba <joaquinvillalba8@gmail.com>
2023-04-04 15:37:48 -0300

    agrego boostrap

--- scripts/validations.js
+++ scripts/validations.js
@@ -1,18 +1,24 @@
-let form = document.querySelector('#registro-form');
-let nombreCompleto = document.querySelector('#nombre-completo');
-let fechaNacimiento = document.querySelector('#fecha-nacimiento');
-let email = document.querySelector('#email');
-let password = document.querySelector('#password');
-let confirmPass = document.querySelector('#confirm-password');
-let genero = document.querySelector('#genero');
-let paisResidencia = document.querySelector('#pais-residencia');
+let form = document.querySelector("#registro-form");
+let nombreCompleto = document.querySelector("#nombre-completo");
+let fechaNacimiento = document.querySelector("#fecha-nacimiento");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let confirmPass = document.querySelector("#confirm-password");
+let genero = document.querySelector("#genero");
+let paisResidencia = document.querySelector("#pais-residencia");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     // Validación de campos obligatorios
-    if (!nombreCompleto.value || !fechaNacimiento.value || !email.value || !password.value || !confirmPass.value) {
-        alert('Todos los campos marcados con * son obligatorios.');
+    if (
+        !nombreCompleto.value ||
+        !fechaNacimiento.value ||
+        !email.value ||
+        !password.value ||
+        !confirmPass.value
+    ) {
+        alert("Todos los campos marcados con * son obligatorios.");
         return;
     }
 
@@ -25,25 +31,25 @@ form.addEventListener('submit', (event) => {
         edad--;
     }
     if (edad < 18) {
-        alert('Debes tener al menos 18 años para registrarte.');
+        alert("Debes tener al menos 18 años para registrarte.");
         return;
     }
 
     // Validación de correo electrónico
     let emailRegex = /^\S+@\S+\.\S+$/;
     if (!email.value.match(emailRegex)) {
-        alert('El correo electrónico no es válido.');
+        alert("El correo electrónico no es válido.");
         return;
     }
 
     // Validación de contraseñas coincidentes
     if (password.value !== confirmPass.value) {
-        alert('Las contraseñas no coinciden.');
+        alert("Las contraseñas no coinciden.");
         return;
     }
 
     // Obtener lista de usuarios existente del localStorage
-    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
     // Crear objeto de usuario
     let usuario = {
@@ -58,49 +64,39 @@ form.addEventListener('submit', (event) => {
     usuarios.push(usuario);
 
     // Guardar lista de usuarios en localStorage
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
     // Mostrar mensaje de confirmación
-    alert('Registro exitoso!');
+    alert("Registro exitoso!");
 });
+
+// Mostrar Tabla
+
+const tablaRegistros = document.createElement('table');
+tablaRegistros.classList.add('table', 'table-striped', 'table-hover');
+const encabezadoTabla = document.createElement('thead');
+const filaEncabezado = document.createElement('tr');
+
+filaEncabezado.innerHTML = '<th>N°</th><th>Nombre completo</th><th>Correo electrónico</th>';
+encabezadoTabla.appendChild(filaEncabezado);
+tablaRegistros.appendChild(encabezadoTabla);
+
+const contenedor = document.querySelector('.container-fluid');
+contenedor.appendChild(tablaRegistros);
+
+tablaRegistros.style.margin = 'auto';
+
+const registros = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+registros.forEach((registro, index) => {
+    const fila = document.createElement('tr');
+    fila.innerHTML = `<td>${index + 1}</td><td>${registro.nombreCompleto}</td><td>${registro.email}</td>`;
+    tablaRegistros.appendChild(fila);
+});
+
+tablaRegistros.style.display = 'none';
+
 const botonMostrarRegistros = document.querySelector('#mostrar-registros');
-
 botonMostrarRegistros.addEventListener('click', () => {
-    const registros = JSON.parse(localStorage.getItem('usuarios')) || [];
-
-    const tablaRegistros = document.createElement('table');
-    tablaRegistros.classList.add('table', 'usuarios-table');
-    const encabezadoTabla = document.createElement('thead');
-    const filaEncabezado = document.createElement('tr');
-    const encabezadoId = document.createElement('th');
-    const encabezadoNombre = document.createElement('th');
-    const encabezadoEmail = document.createElement('th');
-
-    encabezadoId.textContent = 'ID';
-    encabezadoNombre.textContent = 'Nombre completo';
-    encabezadoEmail.textContent = 'Correo electrónico';
-
-    filaEncabezado.appendChild(encabezadoId);
-    filaEncabezado.appendChild(encabezadoNombre);
-    filaEncabezado.appendChild(encabezadoEmail);
-    encabezadoTabla.appendChild(filaEncabezado);
-    tablaRegistros.appendChild(encabezadoTabla);
-
-    registros.forEach((registro, index) => {
-        const fila = document.createElement('tr');
-        const celdaId = document.createElement('td');
-        const celdaNombre = document.createElement('td');
-        const celdaEmail = document.createElement('td');
-
-        celdaId.textContent = index + 1;
-        celdaNombre.textContent = registro.nombreCompleto;
-        celdaEmail.textContent = registro.email;
-
-        fila.appendChild(celdaId);
-        fila.appendChild(celdaNombre);
-        fila.appendChild(celdaEmail);
-        tablaRegistros.appendChild(fila);
-    });
-
-    document.body.appendChild(tablaRegistros);
+    tablaRegistros.style.display = tablaRegistros.style.display === 'none' ? 'table' : 'none';
 });
